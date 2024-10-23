88066124e4b1375dd8c0e450d82e5cec8bd3a355
Zhou <108514246+YuSheng-Zhou@users.noreply.github.com>
2024-10-23 20:27:52 +0800

    Add files via upload

---
+++ ReconFormer/data.py
@@ -0,0 +1,81 @@
+import os
+import utils
+import numpy as np
+import transforms
+from torch.utils import data
+import torch
+
+
+class Dataset(data.Dataset):
+
+    def __init__(self, datapath, down_scale, keep_center):
+
+        self.down_scale = down_scale
+        self.filepath, self.filename = utils.get_filepath(datapath)
+        self.center_ratio = keep_center
+        self.mask = self.create_mask()
+
+    def create_mask(self):
+
+        fullysampled_kspace = np.load(self.filepath[0])['fullysampled_kspace']
+        idx_lower = int((1 - self.center_ratio) * fullysampled_kspace.shape[-1] / 2)
+        idx_upper = int((1 + self.center_ratio) * fullysampled_kspace.shape[-1] / 2)
+        mask = np.zeros_like(fullysampled_kspace[0], dtype=np.float32)
+        mask[..., ::self.down_scale] = 1
+        mask[..., idx_lower:idx_upper] = 1
+
+        return np.stack((mask, mask), axis=-1)
+
+    def __len__(self):
+        return len(self.filepath)
+
+    def __getitem__(self, item):
+        # fully sampled kspace
+        fullysampled_kspace = np.load(self.filepath[item])['fullysampled_kspace']
+        # fully sampled image
+        fully_image = utils.normalize(np.sqrt(np.sum([utils.ifft(k) ** 2 for k in fullysampled_kspace], axis=0)), 2e-6, 1e-8)
+        fullysampled_kspace = utils.fft(fully_image).astype(np.complex64)
+
+        kspace = transforms.to_tensor(fullysampled_kspace)
+        # Apply mask
+        mask = torch.from_numpy(self.mask)
+        masked_kspace = kspace * mask
+        # Inverse Fourier Transform to get zero filled solution
+        image = transforms.ifft2(masked_kspace)
+
+        target = transforms.ifft2(kspace)
+
+        image = image.permute(2, 0, 1)
+        target = target.permute(2, 0, 1)
+        masked_kspace = masked_kspace.permute(2, 0, 1)
+        mask = mask.permute(2, 0, 1)
+
+        return image, target, self.filename[item], mask, masked_kspace
+
+
+def get_dataloader(config, evaluation=False, shuffle=True):
+
+    if not evaluation:
+        train_ds = Dataset(datapath=os.path.join(config.datapath, 'train'),
+                           down_scale=config.down_scale,
+                           keep_center=config.keep_center)
+        val_ds = Dataset(datapath=os.path.join(config.datapath, 'val'),
+                         down_scale=config.down_scale,
+                         keep_center=config.keep_center)
+
+        train_loader = cycle(data.DataLoader(dataset=train_ds, batch_size=config.batch_size, pin_memory=True, shuffle=shuffle))
+        val_loader = data.DataLoader(dataset=val_ds, batch_size=config.batch_size, pin_memory=True, shuffle=shuffle)
+        return train_loader, val_loader
+    else:
+        eval_ds = Dataset(datapath=os.path.join(config.datapath, 'test'),
+                          down_scale=config.down_scale,
+                          keep_center=config.keep_center)
+        eval_loader = data.DataLoader(dataset=eval_ds, batch_size=1, pin_memory=True, shuffle=False)
+        return eval_loader
+
+
+def cycle(dl):
+
+    while True:
+        for data in dl:
+            yield data
---
+++ ReconFormer/run.py
@@ -0,0 +1,146 @@
+import os
+import data
+import utils
+from models.Recurrent_Transformer import ReconFormer
+import torch
+import numpy as np
+import transforms
+
+
+class configs:
+
+    device = 'cuda:0'
+
+    n_iters = 100000
+    val_step = 1000
+    batch_size = 1
+    lr = 0.0001
+
+    data_type = 'knee'
+
+    down_scale = 4
+    keep_center = 0.08
+
+    def __init__(self):
+
+        self.snap_path = os.path.join("result_1", self.data_type, "scale=%d" % self.down_scale)
+        self.datapath = 'G:\\dataset\\fastMRI_%s\\Reconstruction' % self.data_type
+        self.eval_path = os.path.join(self.datapath, "restored", "ReconFormer", "scale=%d" % self.down_scale)
+        if not os.path.exists(self.snap_path):
+            os.makedirs(self.snap_path)
+
+        if self.data_type == "brain":
+            self.img_size = 384
+        else:
+            self.img_size = 320
+
+        self.log_step = 80 // self.batch_size
+
+
+if __name__ == '__main__':
+
+    config = configs()
+    device = config.device
+    print('using device: %s.' % device)
+
+    # Create directories for experimental logs
+    log_file = open(os.path.join(config.snap_path, 'log.txt'), 'a+')
+
+    model = ReconFormer(
+        in_channels=2, out_channels=2, num_ch=(96, 48, 24), num_iter=5, down_scales=(2, 1, 1.5),
+        img_size=config.img_size, num_heads=(6, 6, 6), depths=(2, 1, 1),
+        window_sizes=(8, 8, 8), mlp_ratio=2., resi_connection='1conv',
+        use_checkpoint=(False, False, True, True, False, False)
+    ).to(device)
+    # loss
+    loss_L1 = torch.nn.L1Loss().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+
+    metric_ssim = 0
+    checkpoint_dir = os.path.join(config.snap_path, 'checkpoint.pth')
+
+    # data loader
+    train_loader, val_loader = data.get_dataloader(config)
+
+    for step in range(config.n_iters):
+
+        model.train()
+        optimizer.zero_grad()
+
+        input, target, fname, mask, masked_kspace = next(train_loader)
+        output = model(input.to(device), masked_kspace.to(device), mask.to(device))
+
+        output = transforms.complex_abs(output)
+        target = transforms.complex_abs(target.to(device))
+
+        loss = loss_L1(output, target)
+        loss.backward()
+        optimizer.step()
+        # torch.cuda.empty_cache()
+
+        if (step + 1) % config.log_step == 0:
+            print('Step: [%d/%d], lr: [%.8f], loss=%.5f'
+                % (step + 1, config.n_iters, optimizer.param_groups[0]['lr'], loss.item()))
+            log_file.write('Step: [%d/%d], lr: [%.8f], loss=%.5f\n'
+                % (step + 1, config.n_iters, optimizer.param_groups[0]['lr'], loss.item()))
+
+        if (step + 1) % config.val_step == 0:
+            model.eval()
+            with torch.no_grad():
+                ssim_eval, psnr_eval, loss_eval = 0, 0, 0
+                for i, batch in enumerate(val_loader):
+                    input, target, fname, mask, masked_kspace = batch
+                    output = model(input.to(device), masked_kspace.to(device), mask.to(device))
+
+                    output = transforms.complex_abs(output).unsqueeze(1)
+                    target = transforms.complex_abs(target.to(device)).unsqueeze(1)
+
+                    loss_eval += loss_L1(output, target).item()
+                    ssim_eval += utils.ssim(output, target).item()
+                    psnr_eval += utils.psnr(output, target).item()
+
+                loss_eval = loss_eval / len(val_loader)
+                ssim_eval = ssim_eval / len(val_loader)
+                psnr_eval = psnr_eval / len(val_loader)
+                print("Step: [%d/%d] Evaluation: loss=%.5f, ssim=%.5f, psnr=%.5f\n" % (step + 1, config.n_iters, loss_eval, ssim_eval, psnr_eval))
+                log_file.write("Step: [%d/%d] Evaluation: loss=%.5f, ssim=%.5f, psnr=%.5f\n" % (step + 1, config.n_iters, loss_eval, ssim_eval, psnr_eval))
+
+                if metric_ssim < ssim_eval:
+                    metric_ssim = ssim_eval
+                    torch.save({
+                        "step": step,
+                        "model": model.state_dict()
+                    }, checkpoint_dir)
+                    print('save weights of step %d' % (step + 1) + '\n')
+                    log_file.write('save weights of step %d' % (step + 1) + '\n')
+
+    # evaluation after training
+    model.eval()
+    if not os.path.exists(config.eval_path):
+        os.makedirs(config.eval_path)
+    eval_loader = data.get_dataloader(config, evaluation=True)
+    with torch.no_grad():
+        # load best model
+        checkpoint = torch.load(checkpoint_dir, map_location=device)
+        model.load_state_dict(checkpoint['model'])
+        eval_ssim, eval_psnr = 0, 0
+        for i, batch in enumerate(eval_loader):
+            input, target, fname, mask, masked_kspace = batch
+            output = model(input.to(device), masked_kspace.to(device), mask.to(device))
+
+            output = transforms.complex_abs(output).unsqueeze(1)
+            target = transforms.complex_abs(target.to(device)).unsqueeze(1)
+
+            # evaluation metrics
+            eval_ssim += utils.ssim(output, target).item()
+            eval_psnr += utils.psnr(output, target).item()
+            # save output
+            pred_image = output[0, 0].cpu().numpy()
+
+            np.savez(os.path.join(config.eval_path, fname[0]), pred_image=pred_image)
+        # record the mean metrics
+        num = len(eval_loader)
+
+        print('\nEvaluation: downscale=%d ssim=%.4f and psnr=%.4f' % (config.down_scale, eval_ssim / num, eval_psnr / num))
+        log_file.write('\nEvaluation: downscale=%d ssim=%.4f and psnr=%.4f\n' % (config.down_scale, eval_ssim / num, eval_psnr / num))
+    log_file.close()
